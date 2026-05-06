0aad9cc48e1ec1c3a1193e26fb87a3ecb64cd94b
o7xYU <105417287+o7xYU@users.noreply.github.com>
2026-05-06 16:23:57 +0800

    Delete setting directory
    Signed-off-by: o7xYU <105417287+o7xYU@users.noreply.github.com>

--- setting/tts/constants.js
+++
@@ -1,4 +0,0 @@
-export const DEFAULT_TTS_ENDPOINT = 'https://api.siliconflow.cn/v1';
-export const DEFAULT_TTS_MODEL = 'FunAudioLLM/CosyVoice2-0.5B';
-export const DEFAULT_TTS_VOICE = 'FunAudioLLM/CosyVoice2-0.5B:alex';
-export const DEFAULT_TTS_SPEED = 1;
