7c3acc6ec92b0bec4218f54c47f785ebbb590b57
o7xYU <105417287+o7xYU@users.noreply.github.com>
2026-05-06 16:25:23 +0800

    Add files via upload
    Signed-off-by: o7xYU <105417287+o7xYU@users.noreply.github.com>

---
+++ setting/index.js
@@ -0,0 +1,31 @@
+const MODULE_LOADERS = [
+    () => import('./theme.js'),
+    () => import('./avatar.js'),
+];
+
+const MODULE_KEYS = [
+    ['initThemeSettings', 'setting/theme.js'],
+    ['initAvatarSettings', 'setting/avatar.js'],
+];
+
+function assertFunction(moduleName, exportName, exportValue) {
+    if (typeof exportValue !== 'function') {
+        throw new Error(
+            `模块 ${moduleName} 缺少导出的函数 ${exportName}`,
+        );
+    }
+}
+
+export async function loadSettingModules() {
+    const modules = await Promise.all(MODULE_LOADERS.map((loader) => loader()));
+
+    const exportsMap = {};
+    modules.forEach((module, index) => {
+        const [exportName, moduleName] = MODULE_KEYS[index];
+        const exportValue = module?.[exportName];
+        assertFunction(moduleName, exportName, exportValue);
+        exportsMap[exportName] = exportValue;
+    });
+
+    return exportsMap;
+}
